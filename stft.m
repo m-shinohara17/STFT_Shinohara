4f4685dbecf296b126b4038d6d0631dc82d7b4af
m-shinohara17 <st22060kagawa@gmail.com>
2026-04-16 14:47:31 +0900

    コメントを付けました
    コメント、可読性を上げました

--- stft.m
+++ stft.m
@@ -6,40 +6,37 @@ clear; close all; clc;
 signalLength = length(audioSig);
 
 % 各種パラメータを定義
-windowLength = 4096;%窓長
+windowLength = 4096; %窓長
 shiftLength = windowLength / 2;%シフト長
 
-
 % ゼロパディング
-% paddedSig = padarray(padarray(audioSig, windowLength / 2, 0, "pre"), windowLength - 1, 0, "post");
 paddedSig = [zeros(windowLength / 2, 1) ; audioSig ; zeros(windowLength - 1, 1)];
 
 %分割数
 numFlames =  ceil((windowLength / 2 + signalLength) / shiftLength);
+%行列をあらかじめ用意
 S = zeros(windowLength,numFlames);
 
-win = hann(windowLength);
-
+%行列に波形を格納
 for i = 1 : numFlames
     startIndex = 1 + (i-1) * shiftLength;
     endIndex = startIndex + windowLength - 1;
     S(:,i) = paddedSig(startIndex : endIndex);    
 end
 
-
-
+win = hann(windowLength);%窓用意
 winS = win .* S; %窓かけ
 spect = fft(winS); %dft
-ampSpect = abs(spect);
-powerSpect = 20 * log10(ampSpect);
-nBin = size(spect, 1);
-freqAxis = linspace(0, fs, nBin);
-timeAxis = linspace(0, signalLength/fs, numFlames);
+ampSpect = abs(spect);%絶対値を取得
+powerSpect = 20 * log10(ampSpect);%利得に変換
+
+freqAxis = linspace(0, fs, windowLength);%縦軸(周波数)取得
+timeAxis = linspace(0, signalLength/fs, numFlames);%横軸(時間)取得
 
 
 
-imagesc (timeAxis, freqAxis, powerSpect);
-axis xy;
+imagesc (timeAxis, freqAxis, powerSpect); %グラフ表示
+axis xy;%y軸を上下反転
 c = colorbar;
 c.Label.String = ("利得[db]");
 
