e198dc53194c4a605015218c6b4c5600e4718e1a
m-shinohara17 <st22060kagawa@gmail.com>
2026-04-23 00:50:45 +0900

    追加その1
    窓の選択を可能にしました

--- main.m
+++ main.m
@@ -1,13 +1,18 @@
 clear; close all; clc;
 %各種パラメータ定義、calcSTFTから呼び出し、グラフの表示
-%引数は「audioLength」,「fs」「windowLength」,「shiftLength」,返り値はS
+%引数は「audioLength」,「fs」「windowLength」,「shiftLength」,
+%「windowType」,返り値はS
 
-%変数の定義
+% 変数の定義
 inputFileName = "sig1.wav";
 windowLength = 4096;
 shiftLength = windowLength / 2; 
+% 窓関数の定義 
+% 方形窓，ハン窓，ハミング窓，ブラックマン窓をrec,hann,hamming,blackmanと定義
+windowType ="blackman"; 
+
 
 [audioSig,fs] = audioread(inputFileName); % 音声ファイルから変数の取り出し
 
 % 関数の呼び出し
-calcSTFT (audioSig, fs, windowLength, shiftLength);
+calcSTFT (audioSig, fs, windowLength, shiftLength, windowType);
